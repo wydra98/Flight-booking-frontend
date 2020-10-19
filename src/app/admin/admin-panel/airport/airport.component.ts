05ef954d2f5429eeb633ecf912a41702d187174c
Adrian Wydra <wydra.adrian98@gmail.com>
2020-10-19 12:39:45 +0200

    Creating project structure.

---
+++ src/app/admin/admin-panel/airport/airport.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-airport',
+  templateUrl: './airport.component.html',
+  styleUrls: ['./airport.component.css']
+})
+export class AirportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ErrorComponent } from './error/error.component';
+import { AuthComponent } from './auth/auth.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FlightsComponent } from './user/booking/flights/flights.component';
+import { OrderComponent } from './user/booking/order/order.component';
+import { SearchFlightComponent } from './user/booking/search-flight/search-flight.component';
+import { AirlineComponent } from './admin/admin-panel/airline/airline.component';
+import { AirportComponent } from './admin/admin-panel/airport/airport.component';
+import { FlightComponent } from './admin/admin-panel/flight/flight.component';
+import { PassengerComponent } from './admin/admin-panel/passenger/passenger.component';
+import { TripComponent } from './admin/admin-panel/trip/trip.component';
+import { UserComponent } from './admin/admin-panel/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ErrorComponent,
+    AuthComponent,
+    LogInComponent,
+    SignUpComponent,
+    FlightsComponent,
+    OrderComponent,
+    SearchFlightComponent,
+    AirlineComponent,
+    AirportComponent,
+    FlightComponent,
+    PassengerComponent,
+    TripComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
