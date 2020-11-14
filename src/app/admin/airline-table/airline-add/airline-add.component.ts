d334dc6b5d4bce6cbdf476b6fb2d69fa311e8e75
Adrian Wydra <wydra.adrian98@gmail.com>
2020-11-14 21:33:35 +0100

    Add AirlineAdd Panel

--- src/app/admin/airline-table/airline-add/airline-add.component.ts
+++ src/app/admin/airline-table/airline-add/airline-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {DialogService} from "../../../services/dialog.service";
+import {SnackBarComponent} from "../../../snack-bar/snack-bar.component";
+import {Router} from "@angular/router";
+import {AirlineService} from "../airline.service";
 
 @Component({
   selector: 'app-airline-add',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirlineAddComponent implements OnInit {
 
-  constructor() { }
+  public airlineForm: FormGroup;
+
+  constructor(private airlineService: AirlineService,
+              private dialogService: DialogService,
+              private snackbar: SnackBarComponent,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.airlineForm = this.airlineService.createAirlineForm();
+  }
+
+  public onSubmit() {
+    this.dialogService.openConfirmDialog('Czy na pewno chcesz dodać linię lotniczą?')
+      .afterClosed().subscribe(res => {
+      if (res) {
+        this.airlineService.addAirline(this.airlineService.mapToAirline(this.airlineForm)).subscribe(
+          () => {
+            this.snackbar.showSnackbar("Pomyślnie dodano linię lotniczą", 'success')
+            this.router.navigate(['airline']);
+          },
+          (err) => {
+            this.snackbar.showSnackbar(err.error, 'fail');
+          }
+        )
+      }
+    })
   }
 
 }
