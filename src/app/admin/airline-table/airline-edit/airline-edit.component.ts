65ed4ed571af90b026103e1166875aa45c04219c
Adrian Wydra <wydra.adrian98@gmail.com>
2020-11-14 21:48:53 +0100

    Finish AirlinePanel

--- src/app/admin/airline-table/airline-edit/airline-edit.component.ts
+++ src/app/admin/airline-table/airline-edit/airline-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {Airport} from "../../../models/airport";
+import {DialogService} from "../../../services/dialog.service";
+import {SnackBarComponent} from "../../../snack-bar/snack-bar.component";
+import {Router} from "@angular/router";
+import {AirlineService} from "../airline.service";
+import {Airline} from "../../../models/airline";
 
 @Component({
   selector: 'app-airline-edit',
@@ -7,9 +14,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirlineEditComponent implements OnInit {
 
-  constructor() { }
+  public airlineForm: FormGroup;
+  chosenAirline: Airline;
+
+  constructor(private airlineService: AirlineService,
+              private dialogService: DialogService,
+              private snackbar: SnackBarComponent,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.fetchAirline();
+    this.airlineForm = this.airlineService.createAirlineForm();
+  }
+
+  private fetchAirline() {
+    this.airlineService.getChosenAirline().subscribe(
+      (airline: Airline) => {
+        this.chosenAirline = airline
+      }
+    )
+    this.airlineForm = this.airlineService.createAirlineForm();
+  }
+
+  public onSubmit() {
+    this.dialogService.openConfirmDialog('Czy na pewno chcesz zmodyfikować linię lotniczą?')
+      .afterClosed().subscribe(res => {
+      if (res) {
+        this.airlineService.editAirline(this.airlineService.mapToAirlineWithId(this.airlineForm,this.chosenAirline.id)).subscribe(
+          () => {
+            this.snackbar.showSnackbar("Pomyślnie zmodyfikowano linię lotniczą", 'success')
+            this.router.navigate(['airline']);
+          },
+          (err) => {
+            this.snackbar.showSnackbar(err.error, 'fail');
+          }
+        )
+      }
+    })
   }
 
 }
--- src/app/admin/airport-table/airport-table.component.ts
+++ src/app/admin/airport-table/airport-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {  Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
@@ -32,7 +32,7 @@ export class AirportTableComponent implements OnInit {
   }
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name', 'country'];
+  displayedColumns = ['id', 'name', 'country', 'country', 'timezone' ,'longitude', 'latitude','edit','delete'];
 
   ngOnInit() {
     this.airportService.isLoading.next(0);
